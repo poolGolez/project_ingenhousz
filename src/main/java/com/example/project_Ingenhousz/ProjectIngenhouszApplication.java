e10b7cebec74f7527899b44bebd09c3340bc48d6
Pool Golez <loop.edward@gmail.com>
2025-02-24 19:24:46 +0800

    Initial project setup

---
+++ src/main/java/com/example/project_Ingenhousz/ProjectIngenhouszApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_Ingenhousz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectIngenhouszApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectIngenhouszApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project_Ingenhousz/ProjectIngenhouszApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_Ingenhousz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectIngenhouszApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
